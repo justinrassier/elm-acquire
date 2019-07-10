7390d53bf4d12799aad3997380a2cc2f875eeb39
Justin Rassier <justin.rassier@infinitecampus.com>
2019-07-09 21:41:07 -0500

    tailwind + stupid basics of square generation

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (class, src)
+
 
 
 ---- MODEL ----
@@ -36,10 +37,27 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    div [ class "board" ] (viewBoardSquares 4 4)
+
+
+viewBoardSquares : Int -> Int -> List (Html Msg)
+viewBoardSquares cols rows =
+    List.range 0 ((cols * rows) - 1)
+        |> List.map
+            (\v ->
+                div [ class "flex flex-col justify-center h-24 border border-black border-solid text-center" ]
+                    [ text (boardNumberToSquareNumber cols v ++ boardNumberToSquareLetter cols v) ]
+            )
+
+
+boardNumberToSquareNumber : Int -> Int -> String
+boardNumberToSquareNumber cols value =
+    String.fromInt (modBy cols value + 1)
+
+
+boardNumberToSquareLetter : Int -> Int -> String
+boardNumberToSquareLetter cols value =
+    String.fromChar (Char.fromCode (modBy cols value + 65))
 
 
 
